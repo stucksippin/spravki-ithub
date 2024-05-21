54b3f69419a44b1901cf35599f57e8fcaaa5827f
edwardmvp <120681241+edwardmvp@users.noreply.github.com>
2024-05-21 20:20:52 +0300

    update 1.1

--- components/HistorySpravki.jsx
+++ components/HistorySpravki.jsx
@@ -1,15 +1,124 @@
 'use client'
-import React, {useState} from 'react';
-import { Table, Select } from 'antd';
+import React, { useState, useRef } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table, Select } from 'antd';
+import Highlighter from 'react-highlight-words';
 
 export default function History({references}){
     const [dataSource, setDataSource] = useState(references);
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+      };
+      const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+      };
+      const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+          <div
+            style={{
+              padding: 8,
+            }}
+            onKeyDown={(e) => e.stopPropagation()}
+          >
+            <Input
+              ref={searchInput}
+              placeholder={`Search ${dataIndex}`}
+              value={selectedKeys[0]}
+              onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+              onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              style={{
+                marginBottom: 8,
+                display: 'block',
+              }}
+            />
+            <Space>
+              <Button
+                type="primary"
+                onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                icon={<SearchOutlined />}
+                size="small"
+                style={{
+                  width: 90,
+                }}
+              >
+                Search
+              </Button>
+              <Button
+                onClick={() => clearFilters && handleReset(clearFilters)}
+                size="small"
+                style={{
+                  width: 90,
+                }}
+              >
+                Reset
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  confirm({
+                    closeDropdown: false,
+                  });
+                  setSearchText(selectedKeys[0]);
+                  setSearchedColumn(dataIndex);
+                }}
+              >
+                Filter
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  close();
+                }}
+              >
+                close
+              </Button>
+            </Space>
+          </div>
+        ),
+        filterIcon: (filtered) => (
+          <SearchOutlined
+            style={{
+              color: filtered ? '#1677ff' : undefined,
+            }}
+          />
+        ),
+        onFilter: (value, record) =>
+          record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+          if (visible) {
+            setTimeout(() => searchInput.current?.select(), 100);
+          }
+        },
+        render: (text) =>
+          searchedColumn === dataIndex ? (
+            <Highlighter
+              highlightStyle={{
+                backgroundColor: '#ffc069',
+                padding: 0,
+              }}
+              searchWords={[searchText]}
+              autoEscape
+              textToHighlight={text ? text.toString() : ''}
+            />
+          ) : (
+            text
+          ),
+      });
 
 const columns = [
     {
       title: 'ФИО',
       dataIndex: 'initials',
       key: 'initials',
+      ...getColumnSearchProps('initials'),
     },
     {
       title: 'Группа',
@@ -25,14 +134,23 @@ const columns = [
           value: 'ИСП2',
         },
       ],
-      // specify the condition of filtering result
-      // here is that finding the name started with `value`
-      onFilter: (value, record) => record.name.indexOf(value) === 0,
+      onFilter: (value, record) => record.group.startsWith(value),
     },
     {
       title: 'Вид справки',
       dataIndex: 'typeOfReference',
       key: 'typeOfReference',
+      filters: [
+        {
+          text: 'Транспортная справка',
+          value: 'Транспортная справка',
+        },
+        {
+          text: 'Справка об образовании',
+          value: 'Справка об образовании',
+        },
+      ],
+      onFilter: (value, record) => record.typeOfReference.startsWith(value),
     },
     {
       title: 'Дата запроса',
@@ -43,19 +161,6 @@ const columns = [
           dateStyle: 'full',
           timeZone: 'Europe/Moscow',
         }).format(date)}</p>)
-    },
-    {
-      title: 'Статус',
-      dataIndex: 'status',
-      key: 'status',
-      render: (text, record) => (
-        <Select className='w-[165px]' value={record.status} onChange={(value) => handleStatusChange(record.id, value)}>
-          <Option key='requested' value={1}>Запрошена!</Option>
-          <Option key='inProgress' value={2}>Изготавливается!</Option>
-          <Option key='ready' value={3}>Готова!</Option>
-          <Option key='done' value={4}>Выдана!</Option>
-        </Select>
-      ),
     },
   ];
 
--- components/TableReference.jsx
+++ components/TableReference.jsx
@@ -1,11 +1,122 @@
 'use client'
-import React, { useState } from 'react';
-import { Table, Select } from 'antd';
+import React, { useState, useRef } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table, Select } from 'antd';
+import Highlighter from 'react-highlight-words';
 import Filter from './Filter';
 
 const { Option } = Select;
 
 export default function TableReferences({ references }) {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+
+
   const [dataSource, setDataSource] = useState(references);
 
   const handleStatusChange = async (recordId, newStatus) => {
@@ -36,16 +147,47 @@ export default function TableReferences({ references }) {
       title: 'ФИО',
       dataIndex: 'initials',
       key: 'initials',
+      ...getColumnSearchProps('initials'),
     },
     {
       title: 'Группа',
       dataIndex: 'group',
       key: 'group',
+      filters: [
+        {
+          text: 'ИСП1',
+          value: 'ИСП1',
+        },
+        {
+          text: 'ИСП2',
+          value: 'ИСП2',
+        },
+        {
+          text: 'ИСП3',
+          value: 'ИСП3',
+        },
+      ],
+      onFilter: (value, record) => record.group.startsWith(value),
     },
     {
       title: 'Вид справки',
       dataIndex: 'typeOfReference',
       key: 'typeOfReference',
+      filters: [
+        {
+          text: 'Транспортная справка',
+          value: 'Транспортная справка',
+        },
+        {
+          text: 'Справка об образовании',
+          value: 'Справка об образовании',
+        },
+        {
+          text: 'Справка об обучении',
+          value: 'Справка об обучении',
+        },
+      ],
+      onFilter: (value, record) => record.typeOfReference.startsWith(value),
     },
     {
       title: 'Дата запроса',
