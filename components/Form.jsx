64f075f5902b8312fbc0cf55354724bf067d9069
sippin <regulationqq@mail.ru>
2024-06-20 12:44:09 +0300

    update 2

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { signIn } from 'next-auth/react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
@@ -31,14 +32,18 @@ export default function Form() {
 
 
     return (
-        <form className='border p-5 w-1/3 flex flex-col mx-auto' onSubmit={submitHandler}>
-            <input className='p-3 my-3 rounded-md bg-[#F7F7F8]' required type="text" name='email' placeholder="Введите email" />
-            <input className=' p-3 my-3 rounded-md bg-[#F7F7F8]' required type="password" name='password' placeholder="Введите пароль" />
-            <span className='text-[#921CB0] cursor-pointer text-center'>Забыли пароль?</span>
-            <button className='border  py-2 rounded-md w-full mt-7 mx-auto bg-[#921CB0] text-white'>Войти</button>
-            {
-                error && <p className='text-red-400 mt-10'>Введены некорректные данные, проверьте правильность пароля или логина</p>
-            }
-        </form>
+        <>
+
+            <form className='bg-[#F7F7F8] form__login  p-5 w-1/3 flex flex-col mx-auto rounded-xl' onSubmit={submitHandler}>
+                <span className='text-center mb-5'>Войдите в IThub | Справки</span>
+                <input className='p-3 my-3 rounded-md' required type="text" name='email' placeholder="Введите email" />
+                <input className=' p-3 my-3 rounded-md' required type="password" name='password' placeholder="Введите пароль" />
+                <span className='text-[#921CB0] hover:text-[#bf51db] cursor-pointer text-center'><Link href={'/forgotPassword'}>Забыли пароль?</Link></span>
+                <button className='border  py-2 rounded-md w-full mt-7 mx-auto bg-[#921CB0] hover:bg-[#bf51db] text-white'>Войти</button>
+                {
+                    error && <p className='text-red-400 mt-10'>Введены некорректные данные, проверьте правильность пароля или логина</p>
+                }
+            </form>
+        </>
     )
 }
